f11a9219499ea6323d4bedac3dbd84827b20cff9
AlexandrKirichenko <kiri4enkoalexandr@gmail.com>
2020-07-07 10:21:20 +0300

    add layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,42 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+//функция для создания нашего эл-та
+/* <div class="container">
+   <div class="row">
+    <div class="col-12">
+        
+    </div>
+  </div>
+</div> */
+function createNewElement(tag, content=null, classList=null, attr=null){
+
+    const el = document.createElement(tag);
+    (content)
+        ?(
+            typeof content == 'object'?appendChildToParent(el, [content]):el.innerText = content
+        ):'';
+
+
+    el.className = classList?classList:'';
+
+    if(attr && attr.length > 0){
+        attr.map(at => el.setAttribute(at.name, at.value));
+    }
+
+    return el;
+}
+
+
+function appendChildToParent(parent, arrayOfChildren){
+    arrayOfChildren.map(child => parent.appendChild(child));
+    return parent;
+}
+
+const col = createNewElement('div',null,'col-12');
+const row = createNewElement('div',col,'row');
+const container = createNewElement('div',row, 'container');
+
+console.log(container)
+
+appendChildToParent(document.getElementById('app'), [container])
+
